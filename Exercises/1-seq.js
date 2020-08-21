d397399401992abeb8102535c6b4ff7f23fa93a0
vadymshturkhal <vadymshturkhal@gmail.com>
2020-08-21 13:46:49 +0300

    Solve some tasks.

--- Exercises/1-seq.js
+++ Exercises/1-seq.js
@@ -1,5 +1,16 @@
 'use strict';
 
-const seq = f => g => x => 0;
+const seq = fn => {
+  // Solution is too long...
+  this.funcs = this.funcs || [];
+  if (typeof fn === 'number') {
+    for (const func of this.funcs.reverse()) fn = func(fn);
+    return fn;
+  }
+  this.funcs.push(fn);
+  return seq;
+};
 
+// const result = seq(x => x * 2)(x => x + 7)(5); // 24
+// console.log(result);
 module.exports = { seq };
--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,26 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const inner = x => inner.list[x];
+  inner.list = inner.list || [];
+  inner.push = elem => inner.list.push(elem);
+  inner.pop = () => inner.list.pop();
+  return inner;
+};
+
+// const arr = array();
+// arr.push('first');
+// arr.push('second');
+// arr.push('third');
+//
+// console.log(arr(0)); // Выводит: first
+// console.log(arr(1)); // Выводит: second
+// console.log(arr(2)); // Выводит: third
+//
+// console.log(arr.pop()); // Выводит: third
+// console.log(arr.pop()); // Выводит: second
+// console.log(arr.pop()); // Выводит: first
+//
+// console.log(arr.pop()); // Выводит: undefined
 
 module.exports = { array };
